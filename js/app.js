d39e1b9519e5a17a1f5ce7111192907a2f4b5c6f
Shaik Mudassir <sh.mudassir98@gmail.com>
2019-09-18 22:38:36 -0700

    define rows and column and background color

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 const playground = document.getElementById("game-area");
 const ctx = playground.getContext("2d")
 const scoreElement = document.getElementById("score");
+
+const ROW = 20;
+const COL = COLUMN = 10;
+const SQ = squareSize = 30;
+let grd = ctx.createLinearGradient(500,0,700,700);
+          grd.addColorStop(0,"#8bcbf1");
+          grd.addColorStop(1,"white");
+const VACANT = grd; // color of an empty square
